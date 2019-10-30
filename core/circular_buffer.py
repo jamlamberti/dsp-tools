052b251aa3fddca92e63ccbbf3792dd2c932aaf3
Jack Lamberti <jamlamberti@users.noreply.github.com>
2019-10-29 20:24:53 -0400

    Add support for python3 back

--- core/circular_buffer.py
+++ core/circular_buffer.py
@@ -2,40 +2,60 @@ import sys
 _ON_PYTHON3 = sys.version_info >= (3, 0)
 
 
+class CircularBufferIterator(object):
+    def __init__(self, cb):
+        self._cb = cb
+        self._index = 0
+
+    def next(self):
+        if self._index > self._cb.buf_sz:
+            raise StopIteration
+        self._index += 1
+        return self._cb.get(self._index - 1)
+
+    def __next__(self):
+        return self.next()
+
 class CircularBuffer(object):
     def __init__(self, buf_sz, initial_values=None, initial_value=0):
-        self._buf_sz = buf_sz
+        self.buf_sz = buf_sz
         self._write_ptr = 0
 
         if initial_values is None:
             self._buffer = [initial_value for _ in range(buf_sz)]
         else:
-            if len(initial_values) != self._buf_sz:
+            if len(initial_values) != self.buf_sz:
                 raise ValueError('Must specify all initial values')
             self._buffer = [x for x in initial_values]
         self._initial_values = [x for x in self._buffer]
 
     def append(self, val):
         self._buffer[self._write_ptr] = val
-        self._write_ptr = (self._write_ptr + 1) % self._buf_sz
+        self._write_ptr = (self._write_ptr + 1) % self.buf_sz
 
     def __getitem__(self, idx):
         return self.get(idx)
 
     def __iter__(self):
-        self._it_val = 0
-        return self if not _ON_PYTHON3 else iter(self)
+        return CircularBufferIterator(self)
+    #def __cb_iter(self):
+    #    self._it_val = 0
+    #    return self if not _ON_PYTHON3 else self
 
-    def next(self):
-        if self._it_val > self._buf_sz:
-            raise StopIteration
-        self._it_val += 1
-        return self.get(self._it_val - 1)
+    #def next(self):
+    #    if self._it_val > self.buf_sz:
+    #        raise StopIteration
+    #    self._it_val += 1
+    #    return self.get(self._it_val - 1)
+
 
     def get(self, idx):
-        actual_idx = (self._write_ptr - 1 - idx) % self._buf_sz
+        actual_idx = (self._write_ptr - 1 - idx) % self.buf_sz
         return self._buffer[actual_idx]
 
     def flush(self):
         # XXX: flush is probably the wrong thing
         self._buffer = [x for x in self._initial_values]
+
+    #__iter__ = __cb_iter
+
